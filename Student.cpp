3506827a92b6b181bbaad7b8a6ce8a36c834090b
Austin Wong <austinwong@umail.ucsb.edu>
2018-10-13 01:03:04 -0700

    finished lab

--- Student.cpp
+++ Student.cpp
@@ -4,32 +4,29 @@
 Student::Student(int perm, 
 		 std::string lastName, 
 		 std::string firstAndMiddleNames) {
-  // THIS EMPTY CONSTRUCTOR IS A STUB
-  // DELETE THESE COMMENTS AND REPLACE WITH APPROPRIATE CODE
-  // REMEMBER: PURPOSE OF A CONSTRUCTOR IS TO INITIALIZE 
-  // ALL DATA MEMBERS WITH REASONABLE VALUES  
+  this->perm=perm;
+  this->lastName = lastName;
+  this->firstAndMiddleNames = firstAndMiddleNames;
 }
 
 int Student::getPerm() const { 
-  return -999;
+  return perm;
 }
 
 std::string Student::getLastName() const { 
-  return "stub";
+  return lastName;
 }
 
 std::string Student::getFirstAndMiddleNames() const { 
-  return "stub";
+  return firstAndMiddleNames;
 }
 
-std::string Student::getFullName() const { 
-  return "stub";
+std::string Student::getFullName() const {
+  return firstAndMiddleNames + " " + lastName;
 }
 
 std::string Student::toString() const { 
-  return "stub";
-  /*
-  // e.g. [12345,Smith,Malory Logan]
+
   std::ostringstream oss;
   
   oss << "[" 
@@ -37,7 +34,6 @@ std::string Student::toString() const {
       << this->getLastName() << ","
       << this->getFirstAndMiddleNames() << "]";
   return oss.str();
-  */
 
 } 
 
